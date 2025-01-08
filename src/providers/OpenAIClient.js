08a51f0b59ec84be0a4cc12ce51a4a37cdbda392
Danny Ayers <danny.ayers@gmail.com>
2025-01-08 22:52:56 +0100

    reorg

--- src/providers/OpenAIClient.js
+++ src/providers/OpenAIClient.js
@@ -3,12 +3,16 @@ import OpenAI from 'openai';
 import { AIClient, AIError } from '../base-client.js';
 
 export class OpenAIClient extends AIClient {
-    constructor(config) {
+    constructor(config = {}) {
         super(config);
         this.client = new OpenAI({
-            apiKey: config.apiKey,
+            apiKey: config.apiKey || process.env.OPENAI_API_KEY,
             ...config.clientOptions
         });
+
+        if (!this.client.apiKey) {
+            throw new Error('OpenAI API key is required. Provide it in constructor or set OPENAI_API_KEY environment variable.');
+        }
     }
 
     async chat(messages, options = {}) {
